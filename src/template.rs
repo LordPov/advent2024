79c51e9740b7896de109bf79730d8924a9872f8c
Brendon Ryan <brendon.ryan@edgesportsmarkets.com>
2024-12-02 19:28:17 +1100

    add base and template

---
+++ src/template.rs
@@ -0,0 +1,24 @@
+#[allow(dead_code, unused_imports)]
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+    }
+
+    #[test]
+    fn example_2() {
+    }
+
+    #[test]
+    fn part_1() {
+        println!("Part 1: {}", include_str!("../res/day01.txt").len());
+    }
+
+    #[test]
+    fn part_2() {
+        println!("Part 2: {}", include_str!("../res/day01.txt").len());
+    }
+}
